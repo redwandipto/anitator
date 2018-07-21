88ea9d39731950ec302b38fa36b5f67b502a5b20
redwan07 <redwandipto@gmail.com>
2018-07-22 00:54:52 +0600

    Style added for file upload

--- client/src/components/Entry.vue
+++ client/src/components/Entry.vue
@@ -4,7 +4,9 @@
       <div class="col-12 text-center">
         <h2>Anitator</h2>
       </div>
-    </div>
+    </div>  
+
+    
 
     <div class = "row buttons">
       <button class = "btn btn-success"
@@ -20,6 +22,27 @@
     <div :hidden= !isProcessed>
       <!-- <h1>List of the data set</h1> -->
       <div class="holder">
+        
+        <div class="col-md-6">
+          <form method="post" action="#" id="#">
+            <div class="form-group files">
+              <label>Upload Your File </label>
+              <!-- <input type="file" id="file" ref="myFiles" class="form-control" 
+                      @change="previewFiles" multiple> -->
+
+              <input  class = "form-control"
+                      type="file" 
+                      @change="filesChange" multiple>
+              <!-- <input  type="file" 
+                      class="form-control"
+                      :name="uploadFieldName" 
+                      :disabled="isSaving" 
+                      @change="filesChange($event)" 
+                       > -->
+            </div>
+          </form>   
+        </div>
+
         <table>
             <tr v-for="(data, index) in skills" :key='index'>
               {{ data.skill }}
@@ -60,26 +83,139 @@
 <script>
   import {store} from "../store/basic";
 
+
+  const STATUS_INITIAL = 0, STATUS_SAVING = 1, STATUS_SUCCESS = 2, STATUS_FAILED = 3;
+
   export default {
     name: "Entry",
     data: function () {
       return {
         store,
+        fileinput : '',
         isProcessed : false,
         text: null,
-        skills: ["bro", "valo", "achi"]
+        skills: ["a", "b", "c"],
+        uploadedFile: null,
+        uploadError: null,
+        currentStatus: null,
+        uploadFieldName: 'dataset'
       }
     },
-    methods: {
-      setProcessedData: function(val) {
-        this.isProcessed = val;
-        store.setProcessedData(true);
+
+    computed: {
+      isInitial() {
+        return this.currentStatus === STATUS_INITIAL;
+      },
+      isSaving() {
+        return this.currentStatus === STATUS_SAVING;
+      },
+      isSuccess() {
+        return this.currentStatus === STATUS_SUCCESS;
+      },
+      isFailed() {
+        return this.currentStatus === STATUS_FAILED;
       }
+    },
+
+    methods: {
+      reset() {
+        // reset form to initial state
+        this.currentStatus = STATUS_INITIAL;
+        this.uploadedFile = null;
+        this.uploadError = null;
+      },
+
+      filesChange(event) {
+        console.log("Hello world!");
+        console.log(event.target.files[0]);
+        // this.createInput(event.target.files[0]);
+        var file = event.target.files[0];
+        var reader = new FileReader();
+        
+        var data = '';
+
+        reader.onload = function(event) {
+          data = event.target.result
+          printF(data);
+          console.log("LIFE SUCKS --> " + data);
+        }
+
+        console.log("WHAT THE HELL MAN! --> " + data);
+        reader.readAsText(file);
+
+        var dataset = event.target.result
+
+        // this.store.setText(dataset);
+      },
+
+      printF(f) {
+        console.log(f);
+      },
+
+      setProcessedData(val) {
+        this.isProcessed = val;
+        store.setProcessedData(val);
+      }
+    },
+    mounted() {
+      this.reset();
     }
   }
 </script>
 
 <style scoped>
+  
+  /* input file design */
+
+  .files input {
+    outline: 2px dashed #92b0b3;
+    outline-offset: -10px;
+    -webkit-transition: outline-offset .15s ease-in-out, background-color .15s linear;
+    transition: outline-offset .15s ease-in-out, background-color .15s linear;
+    padding: 120px 0px 85px 35%;
+    text-align: center !important;
+    margin: 0;
+    width: 100% !important;
+}
+.files input:focus{     outline: 2px dashed #92b0b3;  outline-offset: -10px;
+    -webkit-transition: outline-offset .15s ease-in-out, background-color .15s linear;
+    transition: outline-offset .15s ease-in-out, background-color .15s linear; border:1px solid #92b0b3;
+ }
+.files{ position:relative}
+.files:after {  pointer-events: none;
+    position: absolute;
+    top: 60px;
+    left: 0;
+    width: 50px;
+    right: 0;
+    height: 56px;
+    content: "";
+    background-image: url(https://image.flaticon.com/icons/png/128/109/109612.png);
+    display: block;
+    margin: 0 auto;
+    background-size: 100%;
+    background-repeat: no-repeat;
+}
+.color input{ background-color:#f1f1f1;}
+.files:before {
+    position: absolute;
+    bottom: 10px;
+    left: 0;  pointer-events: none;
+    width: 100%;
+    right: 0;
+    height: 57px;
+    content: " or drag it here. ";
+    display: block;
+    margin: 0 auto;
+    color: #2ea591;
+    font-weight: 600;
+    text-transform: capitalize;
+    text-align: center;
+}
+
+
+/* ------------------ */
+
   .buttons {
     display: flex;
     flex-direction: row;
